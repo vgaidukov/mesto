ef07d8e66afe9c3a2a135a8b5c29242d515ecf02
Vadim Gaidukov <vgaidukov@gmail.com>
2022-03-22 21:16:45 +0300

    fix: исправить ошибки в html

--- script/index.js
+++ script/index.js
@@ -4,8 +4,8 @@ let closeButton = popupElement.querySelector('.popup__close-button');
 
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let popupName = document.querySelector('.popup__input_profile-name');
-let popupDescription = document.querySelector('.popup__input_profile-description');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupDescription = document.querySelector('.popup__input_type_description');
 
 const ESC_KEY = 'Escape';
 
@@ -14,13 +14,13 @@ function openPopup() {
     popupDescription.value = profileDescription.textContent;
     popupElement.classList.add('popup_opened');
     document.addEventListener('keyup', onDocumentKeyUp);
-    
+
 }
-    
+
 function closePopup() {
     popupElement.classList.remove('popup_opened');
     document.removeEventListener('keyup', onDocumentKeyUp);
-    
+
 }
 
 function onDocumentKeyUp(event){
@@ -38,4 +38,4 @@ function popupSubmitHandler (evt) {
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-popupElement.addEventListener('submit', popupSubmitHandler); 
+popupElement.addEventListener('submit', popupSubmitHandler);
